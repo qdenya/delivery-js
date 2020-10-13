dd43bcb711e44340f5cead2d68b1de7b66595c76
qdenya <63596498+qdenya@users.noreply.github.com>
2020-10-13 13:52:16 +0300

    Вывод карточек товаров

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
+'use strict';
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
-
-cartButton.addEventListener("click", toggleModal);
-close.addEventListener("click", toggleModal);
-
-function toggleModal() {
-  modal.classList.toggle("is-open");
-}
-
-// day1
 const buttonAuth = document.querySelector(".button-auth");
 const modalAuth = document.querySelector(".modal-auth");
 const closeAuth = document.querySelector(".close-auth");
@@ -17,9 +9,19 @@ const logInForm = document.querySelector("#logInForm");
 const loginInput = document.querySelector("#login");
 const userName = document.querySelector(".user-name");
 const buttonOut = document.querySelector(".button-out");
+const cardsRestaurans = document.querySelector(".cards-restaurants");
+
+const containerPromo = document.querySelector(".container-promo");
+const restaurants = document.querySelector('.restaurants');
+const menu = document.querySelector('.menu');
+const logo  = document.querySelector('.logo');
+const cardsMenu = document.querySelector('.cards-menu');
 
 let login = localStorage.getItem('gloDelivery');
 
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
 function toggleModalAuth() {
   modalAuth.classList.toggle("is-open");
 }
@@ -34,7 +36,6 @@ function autorized() {
     localStorage.removeItem("gloDelivery");
     checkAuth();
   }
-  console.log("Auto");
   userName.textContent = login;
   buttonAuth.style.display = "none";
   userName.style.display = 'inline';
@@ -72,4 +73,84 @@ function checkAuth() {
   notAutorized();
 }
 }
+
+
+//day 2
+
+function createCardRestaurant() {
+  const card = `
+  <a class="card card-restaurant">
+    <img src="img/pizza-burger/preview.jpg" alt="image" class="card-image"/>
+    <div class="card-text">
+      <div class="card-heading">
+          <h3 class="card-title">PizzaBurger</h3>
+          <span class="card-tag tag">45 мин</span>
+      </div>
+      <div class="card-info">
+        <div class="rating">4.5</div>
+        <div class="price">От 700 ₽</div>
+        <div class="category">Пицца</div>
+      </div>
+    </div>
+	</a>
+  `;
+  cardsRestaurans.insertAdjacentHTML('beforeend', card);
+}
+
+
+function createCardGood() {
+  const card = document.createElement('div');
+  card.className = 'card';
+  card.insertAdjacentHTML('beforeend', `
+						<img src="img/pizza-plus/pizza-vesuvius.jpg" alt="image" class="card-image"/>
+						<div class="card-text">
+							<div class="card-heading">
+								<h3 class="card-title card-title-reg">Пицца Везувий</h3>
+							</div>
+							<div class="card-info">
+								<div class="ingredients">Соус томатный, сыр «Моцарелла», ветчина, пепперони, перец
+									«Халапенье», соус «Тобаско», томаты.
+								</div>
+							</div>
+							<div class="card-buttons">
+								<button class="button button-primary button-add-cart">
+									<span class="button-card-text">В корзину</span>
+									<span class="button-cart-svg"></span>
+								</button>
+								<strong class="card-price-bold">545 ₽</strong>
+							</div>
+
+  `);
+  cardsMenu.insertAdjacentElement('beforeend',card);
+}
+
+function openGoods(event) {
+  const target = event.target;
+  const restaurant = target.closest('.card-restaurant');
+  if(restaurant) {
+    if(login) {
+      containerPromo.classList.add('hide');
+      restaurants.classList.add('hide');
+      menu.classList.remove('hide');
+      cardsMenu.textContent = '';
+      createCardGood();     
+    } else {
+      toggleModalAuth();
+    }
+  } 
+}
+
+cardsRestaurans.addEventListener('click', openGoods);
+
+logo.addEventListener('click', function() {
+  containerPromo.classList.remove('hide');
+  restaurants.classList.remove('hide');
+   menu.classList.add('hide');
+})
+
+cartButton.addEventListener("click", toggleModal);
+
+close.addEventListener("click", toggleModal);
+
 checkAuth();
+createCardRestaurant();
